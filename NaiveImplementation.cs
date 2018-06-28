636aeef67f361e37a001f4b0ef39032a0271b5c3
Dmitry Tomp <dtomp@devexperts.com>
2018-06-28 15:25:37 +0300

    Ditch bucket, something wrong in naive algorithm

---
+++ NaiveImplementation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Node = DynamicTopoSort.PkAlgorithm.Node;
+
+namespace DynamicTopoSort
+{
+    static class NaiveImplementation
+    {
+        private static void TopoSort(Node cur, Dictionary<Node, bool> states, IList<Node> result)
+        {
+            if (states.TryGetValue(cur, out var b))
+            {
+                if (!b)
+                    throw new InvalidOperationException("A cycle has been encountered");
+                return;
+            }
+
+            states[cur] = false;
+            for (var i = 0; i < cur.Outgoing.Count; i++)
+                TopoSort(cur.Outgoing[i], states, result);
+            states[cur] = true;
+            result.Append(cur);
+        }
+
+        public static List<Node> AddEdge(List<Node> nodes, Node src, Node dest)
+        {
+            var nAnswer = new List<Node>();
+            var states = new Dictionary<Node, bool>();
+            src.Outgoing.Add(dest);
+            dest.Incoming.Add(src);
+            try
+            {
+                for (var i = 0; i < nodes.Count; i++)
+                    TopoSort(nodes[i], states, nAnswer);
+            }
+            catch
+            {
+                src.Outgoing.RemoveAt(src.Outgoing.Count - 1);
+                dest.Incoming.RemoveAt(dest.Incoming.Count - 1);
+                throw;
+            }
+
+            nAnswer.Reverse();
+            for (int i = 0; i < nAnswer.Count; i++)
+                nAnswer[i].Index = i;
+            return nAnswer;
+        }
+    }
+}
---
+++ TestGen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicTopoSort
+{
+    class TestGen
+    {
+        public static IEnumerable<(int, int)> GenerateTest(int n)
+        {
+            var permutation = Enumerable.Range(0, n).ToArray();
+            var rng = new Random();
+            for (int i = 0; i < n - 1; i++)
+            {
+                var idx = rng.Next(i, n);
+                var t = permutation[i];
+                permutation[i] = permutation[idx];
+                permutation[idx] = t;
+            }
+
+            for (int i = rng.Next((int) Math.Min((long) n * (n - 1) / 2, 1000000)); i >= 0; i--)
+            {
+                var x = rng.Next(n);
+                var y = rng.Next(n - 1);
+                if (y == x)
+                    y++;
+                yield return i > 0 ? (permutation[Math.Min(x, y)], permutation[Math.Max(x, y)]) : (permutation[Math.Max(x, y)], permutation[Math.Min(x, y)]);
+            }
+        }
+    }
+}
